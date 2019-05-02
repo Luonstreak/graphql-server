307d5e9e55e94386a96b85134955322ce0239dd1
Lucio Rodriguez <luonstreak@gmail.com>
2019-05-02 13:30:38 -0700

    creates graphql server

--- schema/schema.js
+++ schema/schema.js
@@ -1,5 +1,17 @@
 const graphql = require('graphql');
-const { GraphQLObjectType, GraphQLString, GraphQLInt } = graphql;
+const { 
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLSchema
+} = graphql;
+const _ = require('lodash');
+
+const users = [
+  { id: "23", firstName: 'Bill', age: 40 },
+  { id: "24", firstName: 'Joe', age: 28 },
+  { id: "25", firstName: 'Mario', age: 34 },
+];
 
 const UserType = new GraphQLObjectType({
   name: 'User',
@@ -10,15 +22,35 @@ const UserType = new GraphQLObjectType({
   },
 })
 
-const RootQuery = GraphQLObjectType({
+const RootQuery = new GraphQLObjectType({
   name: 'RootQueryType',
   fields: {
     user: {
       type: UserType,
       args: { id: { type: GraphQLString }},
       resolve(parentValue, args) {
-        
+        return _.find(users, { id: args.id });
       }
     }
   },
 })
+
+const NameQuery = new GraphQLObjectType({
+  name: 'NameQueryType',
+  fields: {
+    user: {
+      type: UserType,
+      args: { firstName: { type: GraphQLString }},
+      resolve(parentValue, args) {
+        return _.find(users, { firstName: args.firstName });
+      }
+    }
+  },
+})
+
+module.exports = new GraphQLSchema({
+  query: RootQuery,
+});
+module.exports = new GraphQLSchema({
+  query: NameQuery,
+});
